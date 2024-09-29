46b2f84d897d58091d5cc1191bd9d7846a85a711
PUTIK-663 <2655704309@qq.com>
2024-09-29 16:11:20 +0800

    Site updated: 2024-09-29 16:11:19

--- js/read-progress-bar.js
+++ js/read-progress-bar.js
@@ -1,12 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var winHeight = window.innerHeight,
-        docHeight = document.documentElement.scrollHeight,
-        progressBar = document.querySelector('#content_progress');
-    progressBar.max = docHeight - winHeight;
-    progressBar.value = window.scrollY;
+//先判断是否为文章页
+let NeedProgressBar = window.location.pathname.includes("article");
+if(NeedProgressBar){
+	document.addEventListener('DOMContentLoaded', function () {
+    		var winHeight = window.innerHeight,
+        	docHeight = document.documentElement.scrollHeight,
+        	progressBar = document.querySelector('#content_progress');
+    		progressBar.max = docHeight - winHeight;
+    		progressBar.value = window.scrollY;
 
-    document.addEventListener('scroll', function () {
-        progressBar.max = document.documentElement.scrollHeight - window.innerHeight;
-        progressBar.value = window.scrollY;
-    });
-});
+    		document.addEventListener('scroll', function () {
+       			progressBar.max = document.documentElement.scrollHeight - window.innerHeight;
+        		progressBar.value = window.scrollY;
+    		});
+	});
+}
